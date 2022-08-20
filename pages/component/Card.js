3e9c48d37d473024ee647625ab284c52208eb67c
JayU...09 <jaydeep.rathore@sulok.in>
2022-08-21 00:38:27 +0530

    card-view-running

--- pages/component/Card.js
+++ pages/component/Card.js
@@ -7,16 +7,52 @@ export default function Card() {
             <div className='main ms-5 my-2'>
                 <h6 className='mb-2 fs-6'>Promo Codes for More Savings</h6>
             </div>
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="..." class="img-fluid rounded-start" alt="..." />
+
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-sm-4'>
+                        <div class="card mb-3" style={{ maxWidth: "540px", maxHeight: "200px" }}>
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img src="../img/card/card_1.png" class="img-fluid rounded-start" alt="..." />
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">#SPA2022<i class="fas fa-clone ms-5"></i></h5>
+                                        <p class="card-text">Use it for extra discount on Spa</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <div className='col-sm-4'>
+                        <div class="card mb-3" style={{ maxWidth: "540px", maxHeight: "200px" }}>
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img src="../img/card/card_2.png" class="img-fluid rounded-start" alt="..." />
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">#HAIR2022<i class="fas fa-clone ms-5"></i></h5>
+                                        <p class="card-text">Use it for extra discount on Hair</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-sm-4'>
+                        <div class="card mb-3" style={{ maxWidth: "540px", maxHeight: "200px" }}>
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img src="../img/card/card_3.png" class="img-fluid rounded-start" alt="..." />
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">#SHOP2022<i class="fas fa-clone ms-5"></i></h5>
+                                        <p class="card-text">Use it for extra discount on Shop</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
